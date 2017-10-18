445fedf0bb7495bd2dbccfed489c101fcbf448fd
Melo <c.spanti93@gmail.com>
2017-10-18 12:59:02 +0200

    Carmelo Spanti

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,28 @@
 package it.unical.test;
 
-public class AthleticGameBasicTest {
+import java.time.Instant;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
+public class AthleticGameBasicTest {
+	
+	@Test
+	public void addArrivalTest() {
+		AthleticGame ag=new AthleticGame("Atletica");
+		ag.addArrival("Melo", Instant.now());
+		
+		Assert.assertEquals(1, ag.getArrivals().size());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void getPartecipantTest() {
+		AthleticGame ag=new AthleticGame("Atletica");
+		ag.addArrival("Melo", Instant.now());
+		
+		ag.getParecipiantTime("Carmelo");
+		
+	}
 }
